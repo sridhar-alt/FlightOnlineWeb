1aa17cbb2ab5a895d3e3162c49c35b32187c26ba
sridhar-alt <sridhar-alt@users.noreply.github.com>
2020-02-12 18:07:05 +0530

    commit 12-2-2020

--- FlightOnline.DAL/FlightRepository.cs
+++ FlightOnline.DAL/FlightRepository.cs
@@ -1,4 +1,5 @@
 ﻿using OnlineFlightBooking.Entity;
+using System;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
@@ -12,41 +13,72 @@ namespace OnlineFlightBooking.DAL
         {
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                sqlConnection.Open();
-                SqlCommand sqlCommand = new SqlCommand("SP_FLIGHT_ADD", sqlConnection);
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                SqlParameter param = new SqlParameter("@FLIGHTNAME", flight.FlightName);
-                sqlCommand.Parameters.Add(param);
-                param = new SqlParameter("@FLIGHTNUMBER", flight.FlightNumber);
-                sqlCommand.Parameters.Add(param);
-                sqlCommand.ExecuteNonQuery();
+                try
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCommand = new SqlCommand("SP_FLIGHT_ADD", sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    SqlParameter param = new SqlParameter("@FLIGHTNAME", flight.FlightName);
+                    sqlCommand.Parameters.Add(param);
+                    param = new SqlParameter("@FLIGHTNUMBER", flight.FlightNumber);
+                    sqlCommand.Parameters.Add(param);
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
             }
         }
         public static void DeleteFlight(int id)
         {
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                sqlConnection.Open();
-                SqlCommand sqlCommand = new SqlCommand("SP_DELETE_FLIGHTDETAILS", sqlConnection);
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                SqlParameter param = new SqlParameter("@FLIGHTID", id);
-                sqlCommand.Parameters.Add(param);
-                sqlCommand.ExecuteNonQuery();
+                try
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCommand = new SqlCommand("SP_DELETE_FLIGHTDETAILS", sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    SqlParameter param = new SqlParameter("@FLIGHTID", id);
+                    sqlCommand.Parameters.Add(param);
+                    sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
             }
         }
         public static DataTable ViewFlightDetails()
         {
            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                string sql = "SP_FLIGHT_DISPLAY";
-                using (SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection))
+                DataTable dataTable = new DataTable();
+                try
                 {
-                    sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
-                    sqlConnection.Open();
-                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
-                    DataTable dataTable = new DataTable();
-                    sqlDataAdapter.Fill(dataTable);
+                    string sql = "SP_FLIGHT_DISPLAY";
+                    using (SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection))
+                    {
+                        sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                        sqlConnection.Open();
+                        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                        sqlDataAdapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+                catch (Exception)
+                {
                     return dataTable;
+                }
+                finally
+                {
+                    sqlConnection.Close();
                 }
             }
         }
@@ -54,13 +86,23 @@ namespace OnlineFlightBooking.DAL
         {
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                sqlConnection.Open();
-                SqlCommand sqlCommand = new SqlCommand("SP_UPDATE_FLIGHTDB", sqlConnection);
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                sqlCommand.Parameters.AddWithValue("@FLIGHTID", id);
-                sqlCommand.Parameters.AddWithValue("@FLIGHTNAME", flightName);
-                sqlCommand.Parameters.AddWithValue("@FLIGHTNUMBER", flightNumber);
-                int i = sqlCommand.ExecuteNonQuery();
+                try
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCommand = new SqlCommand("SP_UPDATE_FLIGHTDB", sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@FLIGHTID", id);
+                    sqlCommand.Parameters.AddWithValue("@FLIGHTNAME", flightName);
+                    sqlCommand.Parameters.AddWithValue("@FLIGHTNUMBER", flightNumber);
+                    int i = sqlCommand.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
             }
         }
     }
--- FlightOnline.DAL/UserRepository.cs
+++ FlightOnline.DAL/UserRepository.cs
@@ -13,28 +13,39 @@ namespace OnlineFlightBooking.DAL
         {
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                sqlConnection.Open();
-                string sql = "SP_LOGIN";
-                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
-                sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
-                SqlParameter param = new SqlParameter("@MOBILENUMBER", mobile);
-                sqlCommand.Parameters.Add(param);
-                param = new SqlParameter("@PASSWORD", password);
-                sqlCommand.Parameters.Add(param);
-                sqlCommand.Parameters.Add("@ROLE", SqlDbType.VarChar, 15);
-                sqlCommand.Parameters["@ROLE"].Direction = ParameterDirection.Output;
-                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
-                if(Convert.ToString(sqlCommand.Parameters["@ROLE"].Value)=="admin")
+                try
                 {
-                    return 1;
+                    sqlConnection.Open();
+                    string sql = "SP_LOGIN";
+                    SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                    sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlParameter param = new SqlParameter("@MOBILENUMBER", mobile);
+                    sqlCommand.Parameters.Add(param);
+                    param = new SqlParameter("@PASSWORD", password);
+                    sqlCommand.Parameters.Add(param);
+                    sqlCommand.Parameters.Add("@ROLE", SqlDbType.VarChar, 15);
+                    sqlCommand.Parameters["@ROLE"].Direction = ParameterDirection.Output;
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                    if (Convert.ToString(sqlCommand.Parameters["@ROLE"].Value) == "admin")
+                    {
+                        return 1;
+                    }
+                    else if (Convert.ToString(sqlCommand.Parameters["@ROLE"].Value) == "user")
+                    {
+                        return 2;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
                 }
-                else if(Convert.ToString(sqlCommand.Parameters["@ROLE"].Value) == "user")
-                {
-                    return 2;
-                }
-                else
+                catch(Exception)
                 {
                     return 0;
+                }
+                finally
+                {
+                    sqlConnection.Close();
                 }
             }
         }
@@ -43,50 +54,72 @@ namespace OnlineFlightBooking.DAL
             int row = 0;
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                sqlConnection.Open();
-                string sql = "SP_USER_PROC_INSERT";
-                using (SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection))
+                try
                 {
-                    sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
-                    SqlParameter param = new SqlParameter("@NAME", user.Name);
-                    sqlCommand.Parameters.Add(param);
-                    param = new SqlParameter("@MOBILENUMBER", user.Mobile);
-                    sqlCommand.Parameters.Add(param);
-                    param = new SqlParameter("@DOB", user.Dob);
-                    sqlCommand.Parameters.Add(param);
-                    param = new SqlParameter("@SEX", user.Sex);
-                    sqlCommand.Parameters.Add(param);
-                    param = new SqlParameter("@USERADDRESS", user.UserAddress);
-                    sqlCommand.Parameters.Add(param);
-                    param = new SqlParameter("@PASSWORD", user.Password);
-                    sqlCommand.Parameters.Add(param);
-                    param = new SqlParameter("@MEMBERROLE", user.Role);
-                    sqlCommand.Parameters.Add(param);
-                    row = sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Open();
+                    string sql = "SP_USER_PROC_INSERT";
+                    using (SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection))
+                    {
+                        sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                        SqlParameter param = new SqlParameter("@NAME", user.Name);
+                        sqlCommand.Parameters.Add(param);
+                        param = new SqlParameter("@MOBILENUMBER", user.Mobile);
+                        sqlCommand.Parameters.Add(param);
+                        param = new SqlParameter("@DOB", user.Dob);
+                        sqlCommand.Parameters.Add(param);
+                        param = new SqlParameter("@SEX", user.Sex);
+                        sqlCommand.Parameters.Add(param);
+                        param = new SqlParameter("@USERADDRESS", user.UserAddress);
+                        sqlCommand.Parameters.Add(param);
+                        param = new SqlParameter("@PASSWORD", user.Password);
+                        sqlCommand.Parameters.Add(param);
+                        param = new SqlParameter("@MEMBERROLE", user.Role);
+                        sqlCommand.Parameters.Add(param);
+                        row = sqlCommand.ExecuteNonQuery();
+                    }
+                    if (row > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
                 }
-            }
-            if (row > 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
+                catch(Exception)
+                {
+                    return false;
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
             }
         }
         public static DataTable ViewFlightDetails()
         {
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                string sql = "SP_FLIGHT_DISPLAY";
-                using (SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection))
+                DataTable dataTable = new DataTable();
+                try
                 {
-                    sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
-                    sqlConnection.Open();
-                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
-                    DataTable dataTable = new DataTable();
-                    sqlDataAdapter.Fill(dataTable);
+                    string sql = "SP_FLIGHT_DISPLAY";
+                    using (SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection))
+                    {
+                        sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                        sqlConnection.Open();
+                        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                        sqlDataAdapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+                catch (Exception)
+                {
                     return dataTable;
+                }
+                finally
+                {
+                    sqlConnection.Close();
                 }
             }
         }
